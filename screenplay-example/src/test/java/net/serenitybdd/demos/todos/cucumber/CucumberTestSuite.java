d2f9cc23533ff06e036351fc203991c67f064058
Nahuel Lemes <nahulem@gmail.com>
2026-03-05 20:23:33 -0300

    feat: add example project

---
+++ screenplay-example/src/test/java/net/serenitybdd/demos/todos/cucumber/CucumberTestSuite.java
@@ -0,0 +1,16 @@
+package net.serenitybdd.demos.todos.cucumber;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("/features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "net.serenitybdd.demos.todos.cucumber.steps")
+//@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@smoke-test and @filtering")
+public class CucumberTestSuite {
+}
